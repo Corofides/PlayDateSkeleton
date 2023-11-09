ed1797eb3468f1f3687fa94b42611c06a54d7119
Corofides <J.l.lendrem@smoothlines.net>
2023-11-09 12:54:06 +0000

    Add main imports and game loop.

--- src/main.lua
+++ src/main.lua
@@ -4,4 +4,11 @@
 --- DateTime: 09/11/2023 10:56
 ---
 
-print("Hello, World!")
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+function playdate.update()
+   --- Main Game Loop
+end
